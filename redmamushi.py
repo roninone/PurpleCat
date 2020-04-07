c378d26769ee7177d59a96f60180ffb8aef690e3
root <root@localhost.localdomain>
2020-04-07 11:24:18 -0400

    code push

--- redmamushi.py
+++ redmamushi.py
@@ -8,5 +8,10 @@ os.system("touch STATUS.txt")
 os.system("echo 'FAILED' > STATUS.txt")
 os.system("touch ow.txt")
 os.system("echo 'SUCCESS' > ow.txt")
-os.system("dd of=./STATUS.txt if=./ow.txt")
+ddex = "dd of=./STATUS.txt if=./ow.txt"
+output = subprocess.Popen([ddex], stdout=subprocess.PIPE, shell=True)
 os.system("echo 'Your system is vulnerable to Mitre Att&ck: DD, data deletion.\n Best practice: disable 'dd'' >> STATUS.txt")
+print(output.stdout.read())
+#define each test in a separate function
+#def create_account():
+	

d4c65099561e1f7422a5bf52876830ad6d72ab93
Dan L <54668354+danl411412@users.noreply.github.com>
2020-04-10 11:56:45 -0400

    code refractoring, anylogs

--- purplecat.py
+++ purplecat.py
@@ -18,13 +18,6 @@ REPORT_FIELD = ["ART TEST:", "DETECTION RESULT:", "MITRE ATT&CK TECHNIQUE:\"http
 REPORT = ["MITRE ATT&CK FRAMEWORK TEST - PERSISTENCE"]
 
 os.system("touch REPORT.txt")
-def authlog_readlines(num):
-    #num = number of lines to read
-    f = open("/var/log/auth.log", "r")
-    list_f = f.readlines()
-    lines = len(list_f) - num
-    tail_lines = list_f[lines:]
-    return tail_lines
 
 def anylog_readlines(num, type):
     #num = number of lines to read
@@ -35,19 +28,9 @@ def anylog_readlines(num, type):
     tail_lines = list_f[lines:]
     return tail_lines
 
-def match_log(num, case_match):
-    loglines = authlog_readlines(num)
-#	print(loglines)
-    result = "NONE"
-    for line in loglines:
-        if case_match in line:
-            result = line
-
-    return result
-
 def match_anylog(num, case_match, logfile):
+	#logfile = file to read
     loglines = anylog_readlines(num, logfile)
-#	print(loglines)
     result = "NONE"
     for line in loglines:
         if case_match in line:
@@ -58,32 +41,31 @@ def match_anylog(num, case_match, logfile):
 def create_account():
     user_add = "useradd -M -N -r -s /bin/bash -c evil_account joe_exotic"
     os.system(user_add)
-    result = match_log(2, "useradd")
+    result = match_anylog(2, "useradd", "auth.log")
+    #result = match_log(2, "useradd")
     REPORT.append(f"{REPORT_FIELD[0]} T1136: Create Account: Allows for persistence on the system.\n{REPORT_FIELD[1]} {result}{REPORT_FIELD[2]}T1136/\"\n")
     os.system("userdel joe_exotic")
 
 def create_account_root():
     user_add = "useradd -o -u 0 -g 0 -M -d /root -s /bin/bash carole_baskin"
-	#user_pass = "echo 'ikilledmyhusband' | passwd --stdin carole_baskin"
-
     os.system(user_add)
-    result = match_log(2, "name=carole_baskin, UID=0, GID=0")
+    #result = match_log(2, "name=carole_baskin, UID=0, GID=0")
+    result = match_anylog(2, "name=carole_baskin, UID=0, GID=0", "auth.log")
     REPORT.append(f"{REPORT_FIELD[0]} T1136: Create New User with Root UID and GID: Allows for persistence on the system.\n{REPORT_FIELD[1]} {result}")
     os.system("userdel -f carole_baskin > /dev/null 2>&1")
 
 def set_uid_gid():
     set_uid = "sudo touch ./src/john_finlay"
     os.system(set_uid)
-
     change_own = "sudo chown root ./src/john_finlay"
     os.system(change_own)
-
     change_mod = "sudo chmod u+s ./src/john_finlay > /dev/null 2>&1" 
     os.system(change_mod)
-
     change_gid = "sudo chmod g+s ./src/john_finlay > /dev/null 2>&1"
-    result = match_log(10, "COMMAND=/usr/bin/chmod u+s ./src/john_finlay")
-    result_2 = match_log(10, "COMMAND=/usr/bin/chmod g+s ./src/john_finlay") 
+    os.system(change_gid)
+
+    result = match_anylog(10, "COMMAND=/usr/bin/chmod u+s ./src/john_finlay", "auth.log")
+    result_2 = match_anylog(10, "COMMAND=/usr/bin/chmod g+s ./src/john_finlay", "auth.log") 
 
     REPORT.append(f"{REPORT_FIELD[0]} T1166: SetUID and SetGID: Allows for persistence on the system - Applications can be run with the privileges of the owning user or group respectively.\n{REPORT_FIELD[1]} {result}")
     os.system("sudo rm ./src/john_finlay > /dev/null 2>&1")
@@ -113,7 +95,7 @@ def issa_trap():
     os.system("chmod +x ./src/trap.sh")
     run_trap = "./src/trap.sh"
     os.system(run_trap)
-    result = match_log(5, "delete user") 
+    result = match_anylog(5, "delete user", "auth.log") 
     REPORT.append(f"{REPORT_FIELD[0]} T1154: TRAP: Allows for persistence on the system - Trap command allows programs and shells to specify commands that will be executed upon receiving interrupt signals.\n{REPORT_FIELD[1]} {result}") 
 
 def t1215_test():
@@ -122,8 +104,8 @@ def t1215_test():
     os.system("cd ./src/t1215_km && sudo insmod t1215_test.ko")
     os.system("sudo rmmod t1215_test")
     run_log = match_anylog(4, "Hello, K3r#3L", "kern.log")
-    exit_log = match_anylog(2, "Goodbye, k3RnE1", "kern.log")
-    REPORT.append(f"{REPORT_FIELD[0]} T1215:Kernel Modules and Extension\n{REPORT_FIELD[1]}\n{run_log}\n{exit_log}")
+    exit_log = match_anylog(2, "Goodbye, k3RnE1", "kern.log").rstrip()
+    REPORT.append(f"{REPORT_FIELD[0]} T1215:Kernel Modules and Extension\n{REPORT_FIELD[1]}\n{run_log}{exit_log}")
     os.system("cd ./src/t1215_km && sudo rm modules.order Module.symvers t1215_test.ko t1215_test.mod.c t1215_test.mod.o t1215_test.o")
     os.system("cd ./src/t1215_km && sudo rm -rf ./.* > /dev/null 2>&1")
 
@@ -161,7 +143,7 @@ if __name__ == "__main__":
     issa_trap()
     t1215_test()
     systemd_service()
-    #local_scheduling()
+    local_scheduling()
 
     #PRINTING OUT THE RESULTS
     with open('REPORT.txt', 'w') as f:
--- src/trap.sh
+++ src/trap.sh
@@ -3,11 +3,7 @@
 trap func exit 
 
 function func () {
-
-        echo "DELETING USER"
         userdel joshua_dial
-        echo "USER DELETED" 
-
 } 
 
 useradd -M -N -r -s /bin/bash -c evil_account joshua_dial 
